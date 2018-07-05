1f67216220dc823f942e58b6c4d616d93e9f6ea0
Takashi Ando <dodo5522@gmail.com>
2018-07-05 10:10:16 +0900

    Add temperature +/- buttons and function

--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,42 @@ interface ITemperature {
   temp: Number;
 }
 
-class H20 extends React.Component {
+class H2O extends React.Component {
+  state: ITemperature = { temp: 0 };
+
   constructor(props) {
     super(props);
+    console.log(JSON.stringify(props));
     this.state = { temp: 15 };
+  }
+
+  onButtonClickPlusOne = () => {
+    this.setState({temp: this.state.temp + 1;});
+  };
+
+  onButtonClickPlusTen = () => {
+    this.setState({temp: this.state.temp + 10});
+  };
+
+  onButtonClickMinusOne = () => {
+    this.setState({temp: this.state.temp - 1});
+  };
+
+  onButtonClickMinusTen = () => {
+    this.setState({temp: this.state.temp - 10});
+  };
+
+  render() {
+    return (
+      <div style={styles}>
+        <h2>温度: {this.state.temp}</h2>
+        <button onClick={this.onButtonClickPlusOne}>+1</button>
+        <button onClick={this.onButtonClickPlusTen}>+10</button>
+        <button onClick={this.onButtonClickMinusTen}>-10</button>
+        <button onClick={this.onButtonClickMinusOne}>-1</button>
+      </div>
+    );
   }
 }
 
-const App = () => (
-  <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {"\u2728"}</h2>
-  </div>
-);
-
-render(<App />, document.getElementById("root"));
+render(<H2O />, document.getElementById("root"));
